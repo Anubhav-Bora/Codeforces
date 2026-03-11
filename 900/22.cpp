4922e9cf69b73d4581365e5fe438b7944e22176a
Anubhav <anubhavbora40@gmail.com>
2026-03-11 16:59:29 +0530

    900

---
+++ 900/22.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        int countone = 0, countzero = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == 0)
+                countzero++;
+            else if (a[i] == 1)
+                countone++;
+        }
+
+        long long ways = (1LL << countzero) * countone;
+        cout << ways << endl;
+    }
+}
